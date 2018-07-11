b4eabf9cef95693e5a18a9996fc21289a07c10b0
Rodrigo <40340513+roDRYgo0@users.noreply.github.com>
2018-07-11 17:11:15 -0600

    Primer commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Java_and_SQLServer
+Proyecto con Java mostrando como usar SQL Server
---
+++ src/java_and_sqlserver/Java_and_SQLServer.java
@@ -0,0 +1,9 @@
+package java_and_sqlserver;
+
+public class Java_and_SQLServer {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
